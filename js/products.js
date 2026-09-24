0e9ecfc70f634d593c4a466020242e1b9ff1506e
arupra6 <arupra6@live.com>
2026-09-24 15:45:39 +0800

    Create products.js

---
+++ js/products.js
@@ -0,0 +1,50 @@
+const products = [
+    {
+        id: 1,
+        name: "7 cm Electric Sparklers",
+        tamilName: "7 செ.மீ. சாதா கம்பி",
+        category: "Sparklers",
+        price: 24,
+        unit: "Pkt"
+    },
+    {
+        id: 2,
+        name: "7 cm Colour Sparklers",
+        tamilName: "7 செ.மீ. கலர் கம்பி",
+        category: "Sparklers",
+        price: 26,
+        unit: "Pkt"
+    },
+    {
+        id: 29,
+        name: "Ground Chakkar Big",
+        tamilName: "தரைச்சக்கரம் பெரியது",
+        category: "Ground Chakkars",
+        price: 80,
+        unit: "Pkt"
+    },
+    {
+        id: 42,
+        name: "Flower Pots Small",
+        tamilName: "பூச்சட்டி சிறியது",
+        category: "Flower Pots",
+        price: 110,
+        unit: "Pkt"
+    },
+    {
+        id: 83,
+        name: "Bullet Bomb",
+        tamilName: "புல்லட் பாம்",
+        category: "Bombs",
+        price: 60,
+        unit: "Pkt"
+    },
+    {
+        id: 177,
+        name: "ICE AGE MINIONS 19 ITEMS",
+        tamilName: "ஐஸ் ஏஜ் மினியன்ஸ்",
+        category: "Gift Box",
+        price: 275,
+        unit: "Box"
+    }
+];
